4fab1ea9de976f3f6ec48c2fd2f95839826eeab8
Thiry Maximilien <mthiry1@gmail.com>
2024-01-08 16:40:12 +0100

    add form contact with EmailJS

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,6 +5,7 @@ import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import ContactForm from "@/components/common/formcontact"; // Assurez-vous que le chemin d'importation est correct
+import Image from "next/image";
 
 const ContactPage = () => {
   return (
@@ -18,20 +19,32 @@ const ContactPage = () => {
       }}>
       <h2 className="text-2xl font-bold">Contactez-Moi</h2>
       <p>N&apos;hésitez pas à me contacter pour toute question ou demande!</p>
+      <div className="mb-4"></div>
+      <ContactForm />
       <div className="mb-4">
-        {" "}
-        <ContactForm />
+        <p>
+          Vous pouvez me trouver sur les réseaux sociaux via le lien ci-dessous
+          ou alors en scannant le QR-CODE :
+        </p>
+        <div className="mb-4 max-w-xs mx-auto text-center">
+          <Link href="https://linktr.ee/mthiry1">
+            <a>
+              <Image
+                src="/mthiry1.png"
+                alt="Maximilien Thiry linktree social network"
+                width={100}
+                height={100}
+                layout="responsive"
+              />
+            </a>
+          </Link>
+          <p className="mt-2">
+            <Link href="https://linktr.ee/mthiry1">
+              <a>Vers mes réseaux sociaux</a>
+            </Link>
+          </p>
+        </div>
       </div>
-
-      <p>
-        Vous pouvez également me contacter directement par email ou me trouver
-        sur LinkedIn :
-      </p>
-      <Box sx={{ mt: 4, display: "flex", gap: 2 }}>
-        <Link href="https://linkedin.com/in/maximilienthiry" target="_blank">
-          <LinkedInIcon sx={{ fontSize: 60 }} />
-        </Link>
-      </Box>
     </Box>
   );
 };
